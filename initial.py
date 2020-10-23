237437f1a4d17fc8fc910a92e1915899f84642c7
Rich Jiang <rich.jiang2@gmail.com>
2020-10-23 11:45:13 -0400

    testing git

--- initial.py
+++ initial.py
@@ -2,11 +2,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import pandas as pd
-from keras.layers import Dense, Dropout,LSTM
+import torch as t
+from keras.layers import Dense, Dropout, LSTM
 from keras.models import Model
 from keras.layers import Input, Dense
 
-data = pd.read_csv("AAPL.csv", usecols=[1,2,3,4,6,7,8])
+data = pd.read_csv("AAPL.csv", usecols=[1, 2, 3, 4, 6, 7, 8])
 
 opens = data['Open']
 highs = data['High']
@@ -14,8 +15,9 @@ lows = data['Low']
 close = data['Close']
 volume = data['Volume']
 twen_d = data['20_day'] #20 day moving averages
-fif_d = data['50_day']    #50 day moving aberages
+fif_d = data['50_day']    #50 day moving averages
 
 
 
 print("Starting Game")
+print("testing git")
