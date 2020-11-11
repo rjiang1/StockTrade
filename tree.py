e6660db6f815c2bec83b2fb7fff6c2ea484bf126
Rich Jiang <rich.jiang2@gmail.com>
2020-11-11 13:52:00 -0500

    cleaning code. no hard changes

--- tree.py
+++ tree.py
@@ -1,13 +1,14 @@
 import player as p
 import numpy as np
 import pandas as pd
+import random
 
 data = pd.read_csv("AAPL.csv", usecols=[1, 2, 3, 4, 6, 7, 8])
 
-## Recent game_length days data
-##df = data.tail(game_length+1).copy()
+# Recent game_length days data
+# df = data.tail(game_length+1).copy()
 
-##stockObject = p.Stock('Apple',df.iloc[turns,0])
+# stockObject = p.Stock('Apple',df.iloc[turns,0])
 
 
 class Node:
@@ -15,43 +16,46 @@ class Node:
         self.action = action  # the action here means what action the parent took. The root will be hold by default
         self.cash = player.cash  # the portfolio value to at that point in the node
         self.children = []  # this is going to be filled with Node() objects
+        self.player = player
 
 
 def perform_action(choice, playerObj, stockObject, shares):
     if choice == 'S' or choice == 's':
-        #n_shares_sold = 500/float(df.iloc[turn][0])
+
         n_shares_sold = shares
-        playerObj.sell(stockObject,int(n_shares_sold),'Apple')
+        playerObj.sell(stockObject, int(n_shares_sold), 'Apple')
 
     elif choice == 'B' or choice == 'b':
-        #n_shares_sold = 500/float(df.iloc[turn][0])
+
         n_shares_bought = shares
-        playerObj.buy(stockObject,int(n_shares_bought))
+        playerObj.buy(stockObject, int(n_shares_bought))
 
     elif choice == 'H' or choice == 'h':
-            continue
+        pass
   
-    return Node()  # that has taken the action
+    return Node(action=choice, player=playerObj.copy())  # that has taken the action
 
 
 def take_beams(k, array):  # for each level in the array, this function takes the k best
     # in this function we will also need to apply the heuristic to take the k best nodes
     return []
 
+
 def random_take_beam(k,array):  # for each level in the array, this function takes k nodes randomly
     num_node = k 
-    label_list = [i for i in range(len(array))] # [0, 1, 2, 3, 4,...,len(array)]
-    label_list_random = random.sample(label_list, num_node) # choose k labels randomly from the label list
+    label_list = [i for i in range(len(array))]  # [0, 1, 2, 3, 4,...,len(array)]
+    label_list_random = random.sample(label_list, num_node)  # choose k labels randomly from the label list
 
-    nodes_random = array[label_list_random,:] # array([node[label_random_1], node[label_random_2],...,node[label_random_k]])
+    nodes_random = array[label_list_random, :]  # array([node[label_random_1], node[label_random_2],...,node[label_random_k]])
     return nodes_random
+
 
 def doable(act,stockObj, player, shares):  # to check if an action is viable
     if act == 'b':
         if shares <= 0:
             print("Can't by 0 or less shares")
             return False
-        elif stokObj.price * shares > player.cash:
+        elif stockObj.price * shares > player.cash:
             print("Not enough cash")
             return False
     if act == 's':
@@ -72,8 +76,8 @@ def beam_search(root, k, game_length):  # k is the beam size
             for act in ['b', 's', 'h']:  # buy sell hold
                 shares = 500/float(df.iloc[level][0])
                 if doable(act, stockObject, player, shares):
-                    child = perform_action(act,node.copy(), stockObject, shares)
-                    child = Node(action=act, player=node.copy())  # will need to adjust player based on action to above
+
+                    child = perform_action(act, node, stockObject, shares)
                     node.children.append(child)
                     next_q.append(child)
 
@@ -81,6 +85,7 @@ def beam_search(root, k, game_length):  # k is the beam size
         if level == game_length-1:
             return take_beams(1, next_q)  # in the end of the game we will take the best node
         next_q = []
+
 
 def basic_AI(root, k, game_length):  # k is the beam size
     current_q = [root]  # we will iterate through this to create child
@@ -98,9 +103,9 @@ def basic_AI(root, k, game_length):  # k is the beam size
                     node.children.append(child)
                     next_q.append(child)
 
-        current_q = random_take_beams(k, next_q)
+        current_q = random_take_beam(k, next_q)
         if level == game_length-1:
-            return random_take_beams(1, next_q)  # in the end of the game we will take one node randomly
+            return random_take_beam(1, next_q)  # in the end of the game we will take one node randomly
         next_q = []
 
 """
