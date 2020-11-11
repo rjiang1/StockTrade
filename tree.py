00234c884bb630f28133df3c22b31f84331c40b3
dwn1998 <42262393+dwn1998@users.noreply.github.com>
2020-11-11 17:20:19 +0800

    Update tree.py
    ADD def basic_AI(root, k, game_length) which is very similar to def beam_search(root, k, game_length), and def random_take_beam(k,array) to take k nodes randomly in each level.

--- tree.py
+++ tree.py
@@ -20,6 +20,13 @@ def take_beams(k, array):  # for each level in the array, this function takes th
     # in this function we will also need to apply the heuristic to take the k best nodes
     return []
 
+def random_take_beam(k,array):  # for each level in the array, this function takes k nodes randomly
+    num_node = k 
+    label_list = [i for i in range(len(array))] # [0, 1, 2, 3, 4,...,len(array)]
+    label_list_random = random.sample(label_list, num_node) # choose k labels randomly from the label list
+
+    nodes_random = array[label_list_random,:] # array([node[label_random_1], node[label_random_2],...,node[label_random_k]])
+    return nodes_random
 
 def doable():  # to check if an action is viable
     return True
@@ -44,6 +51,24 @@ def beam_search(root, k, game_length):  # k is the beam size
             return take_beams(1, next_q)  # in the end of the game we will take the best node
         next_q = []
 
+def basic_AI(root, k, game_length):  # k is the beam size
+    current_q = [root]  # we will iterate through this to create child
+    next_q = []  # we will que children nodes here
+
+    for level in range(game_length):
+        while current_q:
+            node = current_q.pop()
+            for act in ['b', 's', 'h']:  # buy sell hold
+                if doable:
+
+                    child = Node(action=act, player=node.copy())  # will need to adjust player based on action to above
+                    node.children.append(child)
+                    next_q.append(child)
+
+        current_q = random_take_beams(k, next_q)
+        if level == game_length-1:
+            return random_take_beams(1, next_q)  # in the end of the game we will take one node randomly
+        next_q = []
 
 """
 class TreeNode:
