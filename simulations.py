caeadab72242952dbc1628da75fb0cda3c33a8b5
VaddineniPavankumar <32288760+VaddineniPavankumar@users.noreply.github.com>
2020-11-13 19:15:21 -0500

    Added code for performance and efficiency plots.

--- simulations.py
+++ simulations.py
@@ -21,13 +21,14 @@ if __name__ == "__main__":
     
     for i in range(100):
         random_cash_list.append(random.randint(5000,15000)) # Generating 100 random cash values for 100 simulations with 5 game sizes 
-
+    random_cash_list.sort()
     ## Beam Search AI
 
     for game_len in game_sizes :
         print("Game Length:",game_len)
         result_list = []
         nodes_count_list = []
+        
         for cash in random_cash_list:
             print("starting with cash:",cash)
             player_obj = p.Player('AAPL', cash, {'AAPL': 0})
@@ -40,7 +41,7 @@ if __name__ == "__main__":
             nodes_count_list.append(nodes_processed)
             
         game_len_results.append(result_list)
-        nodes_processed_list.append(nodes_count_list)
+        nodes_processed_list.append(nodes_count_list[0])
  
     print(game_len_results)
     print(nodes_processed_list)
@@ -64,7 +65,7 @@ if __name__ == "__main__":
             nodes_count_list.append(nodes_processed)
             
         game_len_results_basic.append(result_list)
-        nodes_processed_list_basic.append(nodes_count_list)
+        nodes_processed_list_basic.append(nodes_count_list[0])
 
     print(game_len_results_basic)
     print(nodes_processed_list_basic)
@@ -76,34 +77,37 @@ if __name__ == "__main__":
         N = len(size_result)
         ind = np.arange(N) 
         width = 0.5       
-        plt.bar(ind, size_result, width)
+        #plt.bar(ind, size_result, width)
+        plt.figure(figsize = (20,10))
+        plt.plot(ind, size_result)
 
-        plt.ylabel('Profit')
-        plt.title('Beam_Search_AI_performance %02d'%(k))
-        plt.xlabel('Starting cash at each Iteration')
+        plt.ylabel('Profit in USD')
+        plt.title('Beam_Search_AI_performance %02d days'%(k))
+        plt.xlabel('Starting cash at each Iteration (5k-15k) USD')
         plt.xticks(ind + width / 2, random_cash_list, rotation = 'vertical')
         plt.legend(loc='best')
-        plt.savefig('Beam_Per%02d.png'%(k),dpi=300, bbox_inches='tight')
+        plt.savefig('Beam_Per%02d.png'%(k),dpi=300, bbox='tight')
         k+=20
         plt.show()
 
-    k = 5 
+    #k = 5 
     ## Graphs for beam search efficiency
-    for size_result in nodes_processed_list:
-        print(size_result)
-        N = len(size_result)
-        ind = np.arange(N) 
-        width = 0.5       
-        plt.bar(ind, size_result, width)
-
-        plt.ylabel('Number of Nodes Processed')
-        plt.title('Beam_Search_AI_Efficiency %02d'%(k))
-        plt.xlabel('Starting cash at each Iteration')
-        plt.xticks(ind + width / 2, random_cash_list, rotation = 'vertical')
-        plt.legend(loc='best')
-        plt.savefig('Beam_Eff%02d.png'%(k),dpi=300, bbox_inches='tight')
-        k+=20
-        plt.show()
+    #for size_result in nodes_processed_list:
+    #print(size_result)
+    #N = len(size_result)
+    N = len(nodes_processed_list)
+    ind = np.arange(N) 
+    width = 0.5       
+    #plt.bar(ind, nodes_processed_list, width)
+    plt.scatter(ind, nodes_processed_list)
+    plt.ylabel('Number of Nodes Processed for game size days')
+    plt.title('Beam_Search_AI_Efficiency for 5,25,45,65,85 days')
+    plt.xlabel('Game Size in days')
+    plt.xticks(ind + width / 2,game_sizes , rotation = 'vertical')
+    plt.legend(loc='best')
+    plt.savefig('Beam_Eff.png',dpi=300, bbox_inches='tight')
+    k+=20
+    plt.show()
     
     k = 5
     ## Graphs for base line AI performance
@@ -112,34 +116,40 @@ if __name__ == "__main__":
         N = len(size_result)
         ind = np.arange(N) 
         width = 0.5       
-        plt.bar(ind, size_result, width)
+        #plt.bar(ind, size_result, width)
+        plt.figure(figsize = (20,10))
+        plt.plot(ind, size_result)
 
-        plt.ylabel('Profit')
-        plt.title('BaseLine_AI_performance %02d'%(k))
-        plt.xlabel('Starting cash at each Iteration')
+        plt.ylabel('Profit in USD')
+        plt.title('BaseLine_AI_performance %02d days'%(k))
+        plt.xlabel('Starting cash at each Iteration (5k-15k) USD')
         plt.xticks(ind + width / 2, random_cash_list, rotation = 'vertical')
         plt.legend(loc='best')
         plt.savefig('Base_Per%02d.png'%(k),dpi=300, bbox_inches='tight')
         k+=20
         plt.show()
 
-    k = 5    
+    #k = 5    
     ## Graphs for base line AI efficiency
-    for size_result in nodes_processed_list_basic:
-        print(size_result)
-        N = len(size_result)
-        ind = np.arange(N) 
-        width = 0.5       
-        plt.bar(ind, size_result, width)
+    ##for size_result in nodes_processed_list_basic:
+    #print(size_result)
+    #N = len(size_result)
+    N = len(nodes_processed_list_basic)
+    ind = np.arange(N) 
+    width = 0.5       
+    #plt.bar(ind, nodes_processed_list_basic, width)
+    plt.scatter(ind, nodes_processed_list_basic)
+    plt.ylabel('Number of Nodes Processed for game size days')
+    plt.title('BaseLine_AI_Efficiency 5,25,45,65,85 days')
+    plt.xlabel('Game size in days')
+    plt.xticks(ind + width / 2,game_sizes , rotation = 'vertical')
+    plt.legend(loc='best')
+    plt.savefig('Base_Eff.png',dpi=300, bbox_inches='tight')
+    #k+=20
+    plt.show()
 
-        plt.ylabel('Number of Nodes Processed')
-        plt.title('BaseLine_AI_Efficiency %02d'%(k))
-        plt.xlabel('Starting cash at each Iteration')
-        plt.xticks(ind + width / 2, random_cash_list, rotation = 'vertical')
-        plt.legend(loc='best')
-        plt.savefig('Base_Eff%02d.png'%(k),dpi=300, bbox_inches='tight')
-        k+=20
-        plt.show()
+
+    print(random_cash_list)
 
     
 
